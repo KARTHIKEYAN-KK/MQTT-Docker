3c6dc7ab40cf02645e9c8e6c9a3afdf0b03537b4
KARTHIKEYAN THIRUMALAISAMY <99520670+KARTHIKEYAN-KK@users.noreply.github.com>
2023-07-27 14:55:34 +0530

    Add files via upload

---
+++ Grafana/src/constants/app.ts
@@ -0,0 +1,15 @@
+/**
+ * App Information
+ */
+export const AppInfo = {
+  name: 'Volkov Labs',
+  root: '/a/volkovlabs-app',
+};
+
+/**
+ * Routes
+ */
+export const Routes = {
+  community: `${AppInfo.root}`,
+  development: `${AppInfo.root}/development`,
+};
